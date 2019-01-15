7f5540ecdcc2f38dbca59f17de8ceedcf5ea5637
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-15 13:17:46 -0300

    c2-aula-04

--- hangman.c
+++ hangman.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+void show_intro () {
+  printf("\n*****\n");
+  printf("***** WELCOME TO HANGMAN GAME\n");
+  printf("*****\n\n");
+}
+
+void take_a_shot (char shots[26], int tries) {
+  char shot;
+  printf("\n\n");
+  printf("***** (%d) ENTER A LETTER ", tries + 1);
+  scanf(" %c", &shot);
+  shots[tries] = shot;
+}
+
 int main () {
   char secret[20], shot, shots[26];
   int tries = 0;
 
   sprintf(secret, "Cyrax");
-
+  show_intro();
   do {
     printf("\n***** ");
     for (int i = 0; i < strlen(secret); i++) {
@@ -19,10 +33,7 @@ int main () {
       }
       found == 1 ? printf(" %c  ", secret[i]) : printf(" _  ");
     }
-    printf("\n\n");
-    printf("***** (%d) ENTER A LETTER ", tries + 1);
-    scanf(" %c", &shot);
-    shots[tries] = shot;
+    take_a_shot(shots, tries);
     tries++;
   } while (1);
 }
