dad394ce45a431aab703f4ff4f895e8373a02992
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-24 11:50:08 -0300

    refactor hangman.c

--- hangman.c
+++ hangman.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-void choose_your_destiny (char* secret) {
-  sprintf(secret, "scorpion");
+void choose_your_destiny (char* secret_word) {
+  sprintf(secret_word, "scorpion");
 }
 
-int is_successful_shot (char* shots, char shot, int tries) {
-  for (int i = 0; i < tries; i++) {
-    if (shots[i] == shot) {
+int is_correct_attempt (char* attempted_letters, char letter, int attempts) {
+  for (int i = 0; i < attempts; i++) {
+    if (attempted_letters[i] == letter) {
       return 1;
     }
   }
   return 0;
 }
 
-void show_score (char* secret, char* shots, int tries) {
+void show_score (char* secret_word, char* attempted_letters, int attempts) {
   printf("\n***** ");
-  for (int i = 0; i < strlen(secret); i++) {
-    is_successful_shot(shots, secret[i], tries) == 1 ? printf(" %c  ", secret[i]) : printf(" _  ");
+  for (int i = 0; i < strlen(secret_word); i++) {
+    is_correct_attempt(attempted_letters, secret_word[i], attempts) == 1 ? printf(" %c  ", secret_word[i]) : printf(" _  ");
   }
 }
 
@@ -27,22 +27,22 @@ void show_intro () {
   printf("*****\n\n");
 }
 
-void take_a_shot (char* shots, int* tries) {
-  char shot;
+void try_a_letter (char* attempted_letters, int* attempts) {
+  char letter;
   printf("\n\n");
-  printf("***** (%d) ENTER A LETTER ", (*tries) + 1);
-  scanf(" %c", &shot);
-  shots[(*tries)] = shot;
-  (*tries)++;
+  printf("***** (%d) ENTER A LETTER ", (*attempts) + 1);
+  scanf(" %c", &letter);
+  attempted_letters[(*attempts)] = letter;
+  (*attempts)++;
 }
 
 int main () {
-  char secret[20], shot, shots[26];
-  int tries = 0;
-  choose_your_destiny(secret);
+  char secret_word[20], attempted_letters[26];
+  int attempts = 0;
+  choose_your_destiny(secret_word);
   show_intro();
   do {
-    show_score(secret, shots, tries);
-    take_a_shot(shots, &tries);
+    show_score(secret_word, attempted_letters, attempts);
+    try_a_letter(attempted_letters, &attempts);
   } while (1);
 }
