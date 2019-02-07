3bf824202d92f0788e7b238cdb59ae7aa381994c
Caio Rodrigues <rfcaio90@gmail.com>
2019-02-07 13:20:35 -0300

    c2-aula-06

--- hangman.c
+++ hangman.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#include "hangman.h"
 
 void choose_your_destiny (char* secret_word) {
-  sprintf(secret_word, "scorpion");
+  FILE* f = fopen("fighters.txt", "r");
+  if (f == 0) {
+    printf("***** SORRY, FIGHTERS NOT LOADED.\n");
+    exit(1);
+  }
+  int quantity_of_fighters;
+  fscanf(f, "%d", &quantity_of_fighters);
+  srand(time(0));
+  int randomic_number = rand() % quantity_of_fighters;
+  for (int i = 0; i <= randomic_number; i++) {
+    fscanf(f, "%s", secret_word);
+  }
+  fclose(f);
+}
+
+int has_hanged (char* secret_word, char* attempted_letters, int attempts) {
+  int errors = 0;
+  for (int i = 0; i < attempts; i++) {
+    int exists = 0;
+    for (int j = 0; j < strlen(secret_word); j++) {
+      if (attempted_letters[i] == secret_word[j]) {
+        exists = 1;
+        break;
+      }
+    }
+    if (!exists) {
+      errors++;
+    }
+  }
+  return errors > 5;
+}
+
+int has_win (char* secret_word, char* attempted_letters, int attempts) {
+  for (int i = 0; i < strlen(secret_word); i++) {
+    if (!is_correct_attempt(attempted_letters, secret_word[i], attempts)) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 int is_correct_attempt (char* attempted_letters, char letter, int attempts) {
@@ -14,17 +56,17 @@ int is_correct_attempt (char* attempted_letters, char letter, int attempts) {
   return 0;
 }
 
+void show_intro () {
+  printf("\n*****\n");
+  printf("***** WELCOME TO HANGMAN GAME\n");
+  printf("*****\n\n");
+}
+
 void show_score (char* secret_word, char* attempted_letters, int attempts) {
   printf("\n***** ");
   for (int i = 0; i < strlen(secret_word); i++) {
     is_correct_attempt(attempted_letters, secret_word[i], attempts) == 1 ? printf(" %c  ", secret_word[i]) : printf(" _  ");
   }
-}
-
-void show_intro () {
-  printf("\n*****\n");
-  printf("***** WELCOME TO HANGMAN GAME\n");
-  printf("*****\n\n");
 }
 
 void try_a_letter (char* attempted_letters, int* attempts) {
@@ -44,5 +86,5 @@ int main () {
   do {
     show_score(secret_word, attempted_letters, attempts);
     try_a_letter(attempted_letters, &attempts);
-  } while (1);
+  } while (!has_win(secret_word, attempted_letters, attempts) && !has_hanged(secret_word, attempted_letters, attempts));
 }
---
+++ hangman.h
@@ -0,0 +1,14 @@
+
+void choose_your_destiny();
+
+int has_win();
+
+int has_hanged();
+
+int is_correct_attempt(char* attempted_letters, char letter, int attempts);
+
+void show_intro ();
+
+void show_score();
+
+void try_a_letter();
